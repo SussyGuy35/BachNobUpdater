aef99f084c76d5d759473f7620c363605725cbbf
SussyGuy35 <107260269+SussyGuy35@users.noreply.github.com>
2022-08-09 11:38:24 +0700

    Add files via upload

--- updater.py
+++ updater.py
@@ -4,15 +4,18 @@ import os
 
 url_prefix = "https://github.com/SussyGuy35/BachNobServer"
 url = "/raw/main/bachplatformer.zip"
+gamename='bachplatformer'
 
 try:
+    print(f'Welcome to BachNobUpdater. {gamename} is being updated!')
     file = requests.get(url_prefix + url)
-    print('file downloaded!')
+    print('File downloaded!')
     open('package.zip','wb').write(file.content)
     shutil.unpack_archive('package.zip')
-    print('file extracted!')
+    print('File extracted!')
     os.remove("package.zip")
-    print('package file deleted!')
-    print('update success!')
+    print('Package file deleted!')
+    print('Update successful!')
+    input('Press Enter to quit...')
 except:
     print('Error lmao. Maybe u r not connected to the internet.')    

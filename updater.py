7eedc410139f655c139500f56abe27d2f6ff7b23
SussyGuy35 <107260269+SussyGuy35@users.noreply.github.com>
2022-08-20 06:42:44 +0700

    Add files via upload

--- updater.py
+++ updater.py
@@ -1,21 +1,38 @@
-import requests
-import shutil
-import os
+from requests import get
+from shutil import unpack_archive
+from os import remove
+from configparser import ConfigParser
 
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+config = ConfigParser()
 url_prefix = "https://github.com/SussyGuy35/BachNobServer"
 url = "/raw/main/bachplatformer.zip"
 gamename='bachplatformer'
 
+config.read('BachNobUpdater.ini')
+
 try:
-    print(f'Welcome to BachNobUpdater. {gamename} is being updated!')
-    file = requests.get(url_prefix + url)
-    print('File downloaded!')
-    open('package.zip','wb').write(file.content)
-    shutil.unpack_archive('package.zip')
-    print('File extracted!')
-    os.remove("package.zip")
-    print('Package file deleted!')
-    print('Update successful!')
-    input('Press Enter to quit...')
+    path = config.get('Setting', 'path')
 except:
-    print('Error lmao. Maybe u r not connected to the internet.')    
+    path = askdirectory(title='Chọn thư mục game')
+    config.add_section('Setting')
+    config.set('Setting','path',str(path))
+    with open('BachNobUpdater.ini', 'w') as configfile:
+        config.write(configfile)
+
+try:
+    print(f'Cảm ơn bạn đã sử dụng BachNobUpdater v1.1! {gamename} đang được cập nhật!')
+    file = get(url_prefix + url)
+    print('Đã tải gói!')
+    open('package.zip','wb').write(file.content)
+    unpack_archive('package.zip',path)
+    print('Đã giải nén gói!')
+    remove("package.zip")
+    print('Đã xóa gói!')
+    print('Cập nhật thành công!')
+    input('Nhấn Enter để thoát...')
+except:
+    print('Đã có lỗi xảy ra!')
+    input('Nhấn Enter để thoát...')

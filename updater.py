87c4e84eb9d9a0feabf9ab364a0deb7833ba34f8
BachNob <107260269+SussyGuy35@users.noreply.github.com>
2022-10-15 11:29:27 +0700

    Add files via upload

--- updater.py
+++ updater.py
@@ -1,40 +1,80 @@
 print('Chào mừng đến với BachNobUpdater!')
-
 from requests import get
-from shutil import unpack_archive
-from os import remove
+from os import remove, system
 from configparser import ConfigParser
 
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
-
 config = ConfigParser()
-url_prefix = "https://github.com/SussyGuy35/BachNobServer"
-url = "/raw/main/bachplatformer.zip"
-gamename='bachplatformer'
-
-config.read('BachNobUpdater.ini')
-
+url_prefix = "https://raw.githubusercontent.com/sussyguy35/BachNobServer/main/"
+fname = "BachNobUpdater.ini"
+pw = 'tlcktdchlcslotc'
+config.read('BachNobUpdaterConfig.ini')
+# get current_version
 try:
-    path = config.get('Setting', 'path')
+    current_version_str = config.get('Setting', 'current_version')
+    current_version = current_version_str.split(".")
+    current_main, current_minor,current_patch = int(current_version[0]), int(current_version[1]), int(current_version[2])
 except:
-    path = askdirectory(title='Chọn thư mục game')
+    current_main, current_minor,current_patch = 0,0,0
+    current_version_str = f'{current_main}.{current_minor}.{current_patch}'
     config.add_section('Setting')
-    config.set('Setting','path',str(path))
-    with open('BachNobUpdater.ini', 'w') as configfile:
+    config.set('Setting','current_version',current_version_str)
+    with open('BachNobUpdaterConfig.ini', 'w') as configfile:
         config.write(configfile)
-
+print('Vui lòng tắt game nếu game đang chạy! Điều này là cần thiết để trình cập nhật hoạt động đúng!')
+input('Nhấn Enter để xác nhận...')
+# get newest ver
+print('Đang tìm bản cập nhật...')
 try:
-    print(f'Cảm ơn bạn đã sử dụng BachNobUpdater v1.1! {gamename} đang được cập nhật!')
-    file = get(url_prefix + url)
-    print('Đã tải gói!')
-    open('package.zip','wb').write(file.content)
-    unpack_archive('package.zip',path)
-    print('Đã giải nén gói!')
-    remove("package.zip")
-    print('Đã hoàn tất!')
-    print('Cập nhật thành công!')
-    input('Nhấn Enter để thoát...')
+    newest_ver_get = get(url_prefix+fname)
+    open(fname,'wb').write(newest_ver_get.content)
+    config.read(fname)
+    newest_ver_str = config.get('Setting','version')
+    newest_ver = newest_ver_str.split('.')
+    newest_main, newest_minor,newest_patch = int(newest_ver[0]), int(newest_ver[1]), int(newest_ver[2])
+    remove(fname)
+    print(f'Phiên bản hiện tại: {current_version_str}, phiên bản trên máy chủ: {newest_ver_str}')
 except:
-    print('Đã có lỗi xảy ra!')
-    input('Nhấn Enter để thoát...')
+    print('Đã có lỗi xảy ra!')  
+try:
+    if newest_main > current_main:
+        state = 'greater'
+    elif newest_main == current_main and newest_minor> current_minor:
+        state = 'greater'
+    elif newest_main == current_main and newest_minor == current_minor and newest_patch > current_patch:
+        state = 'greater' 
+    elif newest_main == current_main and newest_minor == current_minor and newest_patch == current_patch:
+        state = 'equal'
+    elif newest_main < current_main:
+        state = 'lower'
+    elif newest_main == current_main and newest_minor< current_minor:
+        state = 'lower'
+    elif newest_main == current_main and newest_minor == current_minor and newest_patch < current_patch:
+        state = 'lower'     
+    else :
+        state = 'error'
+except:
+    state = 'error'
+try:
+    if state == 'greater':
+        print('Đang tải xuống...')
+        file = get('https://github.com/SussyGuy35/BachNobServer/raw/main/MyPlatformer-engine.npk')
+        open('MyPlatformer-engine.npk','wb').write(file.content)
+        print('Tải xuống thành công!')
+        print('Đang cài đặt...')
+        system(f'cmd /c "7z.exe x MyPlatformer-engine.npk -p{pw} -y"')
+        remove('MyPlatformer-engine.npk')
+        config.read('BachNobUpdaterConfig.ini')
+        config.set('Setting','current_version',f'1.{newest_ver}')
+        with open('BachNobUpdaterConfig.ini', 'w') as configfile:
+            config.write(configfile)
+        print(f'Cài đặt phiên bản {newest_ver_str} thành công!')
+            
+    elif state == 'equal':
+        print('Phiên bản của bạn bằng với phiên bản trên máy chủ.')
+    elif state == 'lower':
+        print('Phiên bản của bạn cao hơn phiên bản trên máy chủ. How?')
+    elif state == 'error':
+        print('python suck')
+except:
+    print('Đã có lỗi xảy ra')
+input('Nhấn Enter để thoát...')    
